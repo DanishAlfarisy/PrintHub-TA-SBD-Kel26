c75abe41207a8de8231a07d42ae8906b702e5dab
najmaaini1@gmail.com <thirdflowyy@gmail.com>
2026-05-07 20:06:01 +0700

    Adjust flash message width to match page layout

--- resources/views/buyer/orders/index.blade.php
+++ resources/views/buyer/orders/index.blade.php
@@ -2,7 +2,7 @@
 @section('title', 'Pesanan Saya')
 
 @section('content')
-<div class="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+<div class="max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
     <div class="flex items-center justify-between mb-6">
         <div>
             <h1 class="text-2xl font-bold text-gray-800">Pesanan Saya</h1>
